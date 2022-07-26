18964b6e4c2d542999f1e7d27859d6df9911f05b
k0pernicus <dev@0xc0ff33.me>
2022-07-26 21:15:00 +0200

    Initial Commit

---
+++ MetalTest/MetalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTestApp.swift
+//  MetalTest
+//
+//  Created by Antonin on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MetalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
